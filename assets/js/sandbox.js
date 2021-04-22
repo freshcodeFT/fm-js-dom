cb15bdf9c782cc4ac55d3a873183f9da64ea87bf
Nikolay <freshitftm@gmail.com>
2021-04-22 11:39:07 +0300

    Add eventListener once example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('#unique');
 
 const btnHandler = ()=>{
   alert('success');
-  btn.removeEventListener('click', btnHandler);
+  //btn.removeEventListener('click', btnHandler);
 }
 
-btn.addEventListener('click', btnHandler);
+btn.addEventListener('click', btnHandler, {once: true});
