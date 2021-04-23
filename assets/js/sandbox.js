8c2ede266cba3d767783d790aa9a109413787169
Nikolay <freshitftm@gmail.com>
2021-04-23 11:24:16 +0300

    Add data-* task

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,25 +1,22 @@
 "use strict";
 
-const btns = document.querySelectorAll(".link-button");
+const btns = document.querySelectorAll("button");
+
+const clickHandler = ({
+  target: {
+    dataset: { link, port },
+  },
+}) => {
+  console.log(`${link}:${port}`);
+};
 
 for (const btn of btns) {
-  btn.addEventListener(
-    "click",
-    ({
-      target: {
-        dataset: { link, port },
-      },
-    }) => {
-      console.log(`${link}:${port}`);
-    }
-  );
+  btn.addEventListener("click", clickHandler);
 }
 
-const uniqueButton = document.querySelector('#unique');
-
-uniqueButton.addEventListener('click', ({target})=>{ 
-  console.dir(target);
-  /*target.style.background = 'rgb(200, 200, 255)';
-  target.style.padding = '20px';*/
+/*
+  Add styles:
+  target.style.background = 'rgb(200, 200, 255)';
+  target.style.padding = '20px';
   target.style = 'background: rgb(255, 100, 100); padding-left: 30px;'
-});
+*/
