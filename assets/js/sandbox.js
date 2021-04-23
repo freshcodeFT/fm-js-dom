55300a4a7baa35c27532cd0c53df529acf037864
Nikolay <freshitftm@gmail.com>
2021-04-23 10:43:36 +0300

    Add data-* naming example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,37 +1,16 @@
-'use strict';
+"use strict";
 
+const btns = document.querySelectorAll(".link-button");
 
-const [btn1, btn2] = document.querySelectorAll('.changing-button');
-
-/*function createBtnHandler(clicksAmount = 5) {
-  const btnHandler = ({ target: targetBtn }) => {
-
-    if (clicksAmount <= 0) {
-      targetBtn.removeEventListener("click", btnHandler);
-      return;
+for (const btn of btns) {
+  btn.addEventListener(
+    "click",
+    ({
+      target: {
+        dataset: { link, port },
+      },
+    }) => {
+      console.log(`${link}:${port}`);
     }
-
-    alert("success");
-    clicksAmount--;
-  };
-  return btnHandler;
-}*/
-
-
-/*
- Поменять местами содержимое 
- двух кнопок по наведению.
- */
-
-const btnEnterHandler = () => {
-  [btn1.innerText, btn2.innerText] = [btn2.innerText, btn1.innerText];
+  );
 }
-
-btn1.addEventListener('mouseenter', btnEnterHandler);
-btn2.addEventListener('mouseenter', btnEnterHandler);
-
-// for (const btn of btns){
-//   btn.addEventListener('mouseenter', ({target})=>{
-//    target.innerText = 'Changed';
-//   });
-// }
