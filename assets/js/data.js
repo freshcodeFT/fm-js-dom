e7b3978feb748371c9830e40425d77dc7eb91350
Nikolay <freshitftm@gmail.com>
2021-04-26 10:54:30 +0300

    Add data file

---
+++ assets/js/data.js
@@ -0,0 +1,65 @@
+const data = [
+  {
+    id: 1,
+    name: '',
+    description: '',
+    profilePicture:
+      '.com/preview/500-450/content/files/mod.happyhour/e646bf59c47247098a660704f84644a2.jpg',
+  },
+  {
+    id: 2,
+    name: "Anthony's Restaurant & Pizza",
+    description: 'rsxqzmblp',
+    profilePicture:
+      'https://images1.miaminewtimes.com/imager/anthonys-coal-fired-pizza/u/745x420/6405367/5429236.0.jpg',
+  },
+  {
+    id: 3,
+    name: 'Boise Greenbelt Trail',
+    description: 'rhwdedjsl',
+    profilePicture:
+      'https://cf-images.us-east-1.prod.boltdns.net/v1/static/5615998029001/a6c728cd-a576-4efb-93ed-1c83cfdc73f6/3bb237b3-a7f3-4b67-b899-88f6909df3bb/1280x720/match/image.jpg',
+  },
+  {
+    id: 4,
+    name: 'Firefly Restaurant',
+    description: 'roxfrtknd',
+    profilePicture:
+      'https://media-cdn.tripadvisor.com/media/photo-s/18/25/96/89/firefly.jpg',
+  },
+  {
+    id: 5,
+    name: 'Fringale',
+    description: 'rwransgqh',
+    profilePicture:
+      'https://media-cdn.tripadvisor.com/media/photo-s/15/2b/1d/36/nouveau-local-nouvelle.jpg',
+  },
+  {
+    id: 6,
+    name: "Mountain Mike's Pizza - Saratoga",
+    description: 'rzmrjnimy',
+    profilePicture:
+      'https://gonevadacounty.com/wp-content/uploads/2019/07/Mountain-Mikes-Pizza-Hires-Powerhouse-Communications.jpg',
+  },
+  {
+    id: 7,
+    name: 'Philz Coffee',
+    description: 'rezirtngh',
+    profilePicture:
+      'https://i.pinimg.com/originals/19/6b/0c/196b0c16e3fd1fdd6ef110c51f254d59.jpg',
+  },
+  {
+    id: 8,
+    name: 'Raley’s Supermarket',
+    description: 'rhtkrbawm',
+    profilePicture:
+      'https://nonprofitquarterly.org/wp-content/uploads/2016/04/Grocery-co-op-771x552.jpg',
+  },
+  {
+    id: 9,
+    name: 'Rancho San Antonio Open Space Preserve',
+    description: 'rxgrtbgkm',
+    profilePicture:
+      'https://carfreeoutdoors.files.wordpress.com/2009/11/img_1271w.jpg',
+  },
+];
