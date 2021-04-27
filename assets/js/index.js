4475b84f281994fff0898b03df687ddf7d6707ec
Nikolay <freshitftm@gmail.com>
2021-04-27 10:15:53 +0300

    Refactor createPlaceCard

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,66 +3,124 @@
 const cardContainer = document.getElementById("root");
 
 const cardElements = data.map((place) => createPlaceCard(place));
-
 cardContainer.append(...cardElements);
 
 function createPlaceCard(place) {
-  const card = document.createElement("li");
-  card.classList.add("cardWrapper");
 
-  const container = document.createElement("article");
-  container.classList.add("cardContainer");
+  const imageWrapper = createImageWrapper(place);
+  const contentWrapper = createContentWrapper(place);
 
-  const imageWrapper = document.createElement("div");
-  imageWrapper.classList.add("cardImageWrapper");
-  imageWrapper.style.backgroundColor = stringToColor(place.name|| "")
+  // article.cardContainer
+  const container = createElement(
+    "article",
+    { classNames: ["cardContainer"] },
+    imageWrapper,
+    contentWrapper
+  );
 
-  const initials = document.createElement('div');
-  initials.classList.add('initials');
-  //initials.innerText = place.name[0];
-  initials.append(document.createTextNode(place.name[0] || ""));
-
-  const img = document.createElement('img');
-  img.classList.add('cardImage');
-  img.src = place.profilePicture;
-  img.hidden = true;
-
-  img.addEventListener('error', handleImageError);
-  img.addEventListener('load', handleImageLoad);
-
-  imageWrapper.append(initials, img);
-
-  const contentWrapper = document.createElement("div");
-  contentWrapper.classList.add("contentWrapper");
-
-  const name = document.createElement('h3');
-  name.classList.add("cardName");
-  name.append(document.createTextNode(place.name || ""));
-
-  const description = document.createElement("p");
-  description.classList.add("cardDescription");
-  description.append(document.createTextNode(place.description || ""));
-
-  contentWrapper.append(name, description);
-
-  container.append(imageWrapper, contentWrapper);
-  card.append(container);
+  // li.cardWrapper
+  const card = createElement("li", { classNames: ["cardWrapper"] }, container);
 
   return card;
+}
+
+function createImageWrapper({name, profilePicture}) {
+
+  // div.initials
+  const initials = createElement(
+    "div",
+    { classNames: ["initials"] },
+    document.createTextNode(name[0] || "")
+  );
+
+  //img.cardImage
+  const img = createElement("img", {
+    classNames: ["cardImage"],
+    handlers: {
+      error: handleImageError,
+      load: handleImageLoad,
+    },
+  });
+  img.src = profilePicture;
+  img.hidden = true;
+
+  // div.cardImageWrapper
+  const imageWrapper = createElement(
+    "div",
+    {
+      classNames: ["cardImageWrapper"],
+    },
+    initials,
+    img
+  );
+  imageWrapper.style.backgroundColor = stringToColor(name || "");
+
+  return imageWrapper;
+}
+
+function createContentWrapper({name, description}){
+  //h3.cardName
+  const cardName = createElement(
+    "h3",
+    { classNames: ["cardName"] },
+    document.createTextNode(name || "")
+  );
+
+  //p.cardDescription
+  const cardDescription = createElement(
+    "p",
+    { classNames: ["cardDescription"] },
+    document.createTextNode(description || "")
+  );
+
+  //div.contentWrapper
+  const contentWrapper = createElement(
+    "div",
+    {
+      classNames: ["contentWrapper"],
+    },
+    cardName,
+    cardDescription
+  );
+  return contentWrapper;
+}
+
+/**
+ *
+ * @param {string} tagName
+ * @param {object} options
+ * @param {string[]} options.classNames - css classes
+ * @param {object} options.handlers - event handlers
+ * @param  {...Node} children
+ * @returns {HTMLElement}
+ */
+function createElement(
+  tagName,
+  { classNames = [], handlers = {} },
+  ...children
+) {
+  const elem = document.createElement(tagName);
+  elem.classList.add(...classNames);
+
+  for (const [eventType, eventHandler] of Object.entries(handlers)) {
+    elem.addEventListener(eventType, eventHandler);
+  }
+
+  elem.append(...children);
+  return elem;
 }
 
 /*
   EVENT HANDLERS
 */
 
-function handleImageError({target}){
+function handleImageError({ target }) {
   target.remove();
 }
 
-function handleImageLoad({target}){
+function handleImageLoad({ target }) {
   target.hidden = false;
 }
-
 
 /*
   UTILS
@@ -73,10 +131,10 @@ function stringToColor(str) {
   for (var i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  var colour = '#';
+  var colour = "#";
   for (var i = 0; i < 3; i++) {
-    var value = (hash >> (i * 8)) & 0xFF;
-    colour += ('00' + value.toString(16)).substr(-2);
+    var value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).substr(-2);
   }
   return colour;
 }
