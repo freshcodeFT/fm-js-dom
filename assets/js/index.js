7d743d416847c1288221430aba3570c1ecf90c01
Nikolay <freshitftm@gmail.com>
2021-04-27 10:44:15 +0300

    Refactor stringToColor

--- assets/js/index.js
+++ assets/js/index.js
@@ -116,13 +116,13 @@ function handleImageLoad({ target }) {
 */
 
 function stringToColor(str) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  var colour = "#";
-  for (var i = 0; i < 3; i++) {
-    var value = (hash >> (i * 8)) & 0xff;
+  let colour = "#";
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
     colour += ("00" + value.toString(16)).substr(-2);
   }
   return colour;
