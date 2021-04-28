f98bf5ddd2f3f54fc2da2a29a0c70b95e4980a83
Nikolay <freshitftm@gmail.com>
2021-04-28 10:13:55 +0300

    Add task template

---
+++ assets/js/data.js
@@ -0,0 +1,97 @@
+/* 
+
+  FILE READONLY
+
+*/
+const responseData = [
+  {
+    id: 1,
+    firstName: 'Jason',
+    lastName: 'Statham',
+    profilePicture:
+      'https://i.pinimg.com/orignals/24/f5/f8/24f5f8ef9f8af9c7e795ff0ba15f6881.jpg',
+    contacts: [
+      'https://www.facebook.com/JasonStatham/',
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/jasonstatham/?hl=ru',
+    ],
+  },
+  {
+    id: 2,
+    firstName: 'Dwayne',
+    lastName: 'Johnson',
+    profilePicture:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/800px-Dwayne_Johnson_2%2C_2013.jpg',
+    contacts: [
+      'https://www.facebook.com/DwayneJohnson',
+      'https://www.instagram.com/therock/?hl=ru',
+    ],
+  },
+  {
+    id: 3,
+    firstName: 'Emma',
+    lastName: 'Stone',
+    profilePicture:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Emma_Stone_at_he_39th_Mill_Valley_Film_Festival_%28cropped%29.jpg/320px-Emma_Stone_at_the_39th_Mill_Valley_Film_Festival_%28cropped%29.jpg',
+    contacts: [
+      'https://www.facebook.com/DwayneJohnson',
+      'https://www.instagram.com/therock/?hl=ru',
+    ],
+  },
+  {
+    id: 4,
+    firstName: 'Somebody',
+    lastName: 'Tuffak',
+    profilePicture: null,
+    contacts: [
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/therock/?hl=ru',
+    ],
+  },
+  {
+    id: 999,
+    firstName: '',
+    lastName: '',
+    profilePicture:
+      'https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwTUyNzc3Nw@@._V1_.jpg',
+    contacts: [
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/therock/?hl=ru',
+    ],
+  },
+  {
+    id: 5,
+    firstName: 'Scarlett',
+    lastName: 'Johansson',
+    profilePicture:
+      'https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_.jpg',
+    contacts: [
+      'https://www.facebook.com/pages/category/Actor/Scarlett-Johansson-Official-101457158013203/',
+      'https://twitter.com/scarlett_jo',
+      'https://www.instagram.com/scarlett.johansson.fc/?hl=ru',
+    ],
+  },
+  {
+    id: 6,
+    firstName: 'Yevgeny',
+    lastName: 'Ponasenkov',
+    profilePicture:
+      'https://www.factroom.ru/wp-content/uploads/2019/06/10-faktov-o-evgenii-ponasenkove-kotoryj-svodit-vsekh-s-uma-1250x883.jpg',
+    contacts: [
+      'https://www.facebook.com/Ponasenkov',
+      'https://twitter.com/Ponasenkov',
+    ],
+  },
+  {
+    id: 7,
+    firstName: 'Daniel',
+    lastName: 'Day-Lewis',
+    profilePicture:
+      'https://i2.wp.com/comicbookdebate.com/wp-content/uploads/2019/07/There20will20be20blood202-750x460.jpg?resize=750%2C460&ssl=1',
+    contacts: [
+      'https://www.facebook.com/JasonStatham/',
+      'https://twitter.com/realjstatham',
+      'https://www.instagram.com/jasonstatham/?hl=ru',
+    ],
+  },
+];
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,55 +1,26 @@
 "use strict";
-const form = document.getElementById("root-form");
-const list = document.getElementById("root-list");
 
-form.addEventListener("submit", submitHandler);
+/**
+ *
+ * @param {string} tagName
+ * @param {object} options
+ * @param {string[]} options.classNames - css classes
+ * @param {object} options.handlers - event handlers
+ * @param  {...Node} children
+ * @returns {HTMLElement}
+ */
+function createElement(
+  tagName,
+  { classNames = [], handlers = {} },
+  ...children
+) {
+  const elem = document.createElement(tagName);
+  elem.classList.add(...classNames);
 
-const arr = [];
-
-function submitHandler(event) {
-  event.preventDefault();
-  const {
-    target,
-    target: {
-      elements: {
-        email: { value: emailValue },
-      },
-    },
-  } = event;
-  const value = emailValue.trim();
-  if (value && !arr.includes(value)) {
-    arr.push(value);
-    list.append(createListItem(value));
+  for (const [eventType, eventHandler] of Object.entries(handlers)) {
+    elem.addEventListener(eventType, eventHandler);
   }
 
-  target.reset();
+  elem.append(...children);
+  return elem;
 }
-
-function createListItem(value) {
-  const listItem = document.createElement("li");
-
-  const btn = createDeleteListButton(deleteHandler.bind(value));
-  listItem.append(document.createTextNode(value), btn);
-
-  return listItem;
-}
-
-function createDeleteListButton(onDelete) {
-  const btn = document.createElement("button");
-  btn.innerText = "X";
-  btn.addEventListener("click", onDelete);
-  return btn;
-}
-
-function deleteHandler({ target: { parentNode } }) {
-  console.log(this);
-  arr.splice(arr.indexOf(this), 1);
-  parentNode.remove();
-}
-
-/* 1. При каждом сабмите формы - пушить значение инпута в массив */
-/* 2. Очищать форму после каждого сабмита */
-/* 3. Рендерить на странице то, что было введено в форму */
-/* 4. Валидация значения в инпуте при сабмите формы (На пустой инпут) */
-/* 5. Добавить в элементы списка кнопку удаления, которая 
-удалит этот li элемент и соответствующее значение в массиве. */
