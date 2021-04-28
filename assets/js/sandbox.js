de5b0943efba620ab3268b9bd45f9c4a27db1921
Nikolay <freshitftm@gmail.com>
2021-04-28 10:09:17 +0300

    Add function bind example

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function testFun() {
+  console.log(this);
+}
+
+const testObj = {
+  foo: "bar",
+  sum(a, b) {
+    console.log(this);
+    //return a + b;
+  },
+};
+
+testFun();
+
+const testFunWithContext = testFun.bind({});
+
+testFunWithContext();
