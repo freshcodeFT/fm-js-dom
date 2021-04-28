a5d7b30f605d28ffec310158189324987c99e2ec
Nikolay <freshitftm@gmail.com>
2021-04-28 10:27:44 +0300

    Refactor createElement

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,16 +6,30 @@
  * @param {object} options
  * @param {string[]} options.classNames - css classes
  * @param {object} options.handlers - event handlers
+ * @param {object} options.attributes - attributes
  * @param  {...Node} children
  * @returns {HTMLElement}
  */
 function createElement(
   tagName,
-  { classNames = [], handlers = {} },
+  { classNames = [], handlers = {}, attributes = {} } = {},
   ...children
 ) {
   const elem = document.createElement(tagName);
   elem.classList.add(...classNames);
+
+  /*
+  attributes object example
+  {
+    src: "https://example.com",
+    value: "text",
+    name: "textInput",
+  }
+  */
+
+  for( const [attrName, attrValue] of Object.entries(attributes)){
+    elem.setAttribute(attrName, attrValue);
+  }
 
   for (const [eventType, eventHandler] of Object.entries(handlers)) {
     elem.addEventListener(eventType, eventHandler);
--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,1 +1,4 @@
 "use strict";
+
+const url = new URL('https://www.instagram.com/therock/?hl=ru');
+//console.log(url.hostname);
