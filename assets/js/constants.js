ff2cd7708301b7c0aca4b2d1ced63eb0063e038f
Nikolay <freshitftm@gmail.com>
2021-04-30 12:16:11 +0300

    Add createLinks

---
+++ assets/js/constants.js
@@ -0,0 +1,4 @@
+const SOCIAL_ICONS_MAP = new Map()
+.set("www.facebook.com", "fa-facebook-f")
+.set("www.instagram.com", "fa-instagram")
+.set("twitter.com", "fa-twitter");
--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,4 +1,14 @@
 "use strict";
 
-const url = new URL('https://www.instagram.com/therock/?hl=ru');
+const url = new URL("https://www.instagram.com/therock/?hl=ru");
 //console.log(url.hostname);
+
+function createLinks(contacts) {
+  return contacts.map((contact) => {
+    const url = new URL(contact);
+    return createElement("a", {
+      attributes: { href: contact },
+      classNames: [SOCIAL_ICONS_MAP.get(url.hostname) || "fa-adn", "fab", 'socialLink'],
+    });
+  });
+}
